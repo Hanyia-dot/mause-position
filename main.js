065c3d95164bbbc8efd3854ff835c66735e389a0
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 16:00:09 +0200

    add background color

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const h1 = document.querySelector('h1')
 
 document.body.addEventListener('mousemove' , (event) => {
     
-    //h1.textContent = `Pozycja myszki: ${event.clientX} , ${event.clientY}`
-    h1.textContent = `Pozycja myszki: ${event.pageX} , ${event.pageY}`
+    //h1.textContent = `Pozycja myszki: ${event.clientX} , ${event.clientY}`  - względem wyświetlanego okna przeglądarki
+    h1.textContent = `Pozycja myszki: ${event.pageX} , ${event.pageY}` // względem całego dokumentu
+
+    document.body.style.backgroundColor = `rgb(${event.pageX/3}, ${event.pageY/2}, ${event.pageX/event.pageY*10})`
 
 })
