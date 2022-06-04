0b3a08cad4efcfb26f59b95ed0a8c633c6b2ceb9
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-04 15:31:44 +0200

    add an mouse position display

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-document.body.addEventListener('mousemove' , (event) => {
-    console.log(event.clientX, event.clientY)
+const h1 = document.querySelector('h1')
 
+document.body.addEventListener('mousemove' , (event) => {
+    
+    //h1.textContent = `Pozycja myszki: ${event.clientX} , ${event.clientY}`
+    h1.textContent = `Pozycja myszki: ${event.pageX} , ${event.pageY}`
 
 })
